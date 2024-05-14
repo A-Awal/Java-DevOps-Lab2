003bef3b37693ffa1eb92d26e748a96ff7a0c1d1
A-Awal <abdulai.awal@amalitech.org>
2024-05-14 13:23:07 +0000

    Comit

---
+++ Lab2/src/main/java/com/example/Lab2/Lab2Application.java
@@ -0,0 +1,20 @@
+package com.example.Lab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Lab2Application {
+
+	@GetMapping("test")
+	public String GetResult(){
+		return "Hello";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2Application.class, args);
+	}
+
+}
